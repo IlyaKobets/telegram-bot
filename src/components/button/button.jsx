f212b8a4d3801882e715dbf287a5253edc32c52c
Ilya <stebok25@gmail.com>
2023-05-18 19:04:31 +0600

    2

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Button = (props) => {
     return (  
-        <Button className={'button ' + props.className}/>
+        <div>
+            <Button className={'button ' + props.className}/>
+            <Button />
+        </div>
+        
     );
 }
  
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Button from "../button/button";
-
 
 
 const Header = () => {
