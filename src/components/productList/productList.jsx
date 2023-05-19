5ceb100a2f5b6b84fccf4f01f883effa111b1c46
Ilya <stebok25@gmail.com>
2023-05-19 16:22:05 +0600

    1

--- src/components/productList/productList.jsx
+++ src/components/productList/productList.jsx
@@ -1,7 +1,50 @@
+import './productList.css'
+import ProductItem from '../productItem/productItem';
+import { useState } from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
+
+
+const products = [
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синий цвет, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленый цвет, теплая'}
+]
+
+
+
+
+
 const ProductList = () => {
+
+    const [addedItems, setAddedItem] = useState('');
+
+    const {tg} = useTelegram();
+
+    const onAdd = (product) => {
+        
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id === product.id);
+        } else {
+            newItems = [...addedItems, product]
+        }
+        
+        
+        setAddedItem(newItems)
+    }
+
+
     return (  
-        <div>
-            PRODUCTLIST
+        <div className={'list'}>
+            {products.map(item => {
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            })}
         </div>
     );
 }
